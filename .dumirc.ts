3c182f549a394f5169a292ee22839034412225e3
cuiwujie <cuiwujie@autohome.com.cn>
2023-09-22 09:17:10 +0800

    docs: 更新文档

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
-  outputPath: 'docs-dist',
+  outputPath: 'rn-atomcss',
+  publicPath: '/rn-atomcss/',
   themeConfig: {
-    name: 'RN-AtomCSS',
+    name: 'rn-atomcss',
+    logo: '',
   },
+  history: {
+    type: 'hash',
+  },
+  styles: [
+    `body .dumi-default-features-item{text-align: center}`,
+    `body .dumi-default-header-left{width: 250px;}`,
+    `.dumi-default-hero-title{font-size: 100px;}`,
+  ],
 });
